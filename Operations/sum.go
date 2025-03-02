2421ce2334ee5be20709ffb5297ca25848345c2a
Shubhamtrip <shubhamtrip16@gmail.com>
2025-03-03 01:47:08 +0530

    Basics added

---
+++ Operations/sum.go
@@ -0,0 +1,14 @@
+package operations
+
+func Sum(a int, b int) int {
+	return a + b
+}
+
+// import "fmt"
+
+// func main() {
+// 	var a int = 10
+// 	var b int = 20
+// 	var c int = a + b
+// 	fmt.Println("Sum of a and b is ", c)
+// }
---
+++ helloworld.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	operations "Golang/Operations"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello World!")
+
+	var sum int = operations.Sum(10, 20)
+
+	fmt.Println("Sum of 10 and 20 is: ", sum)
+
+}
+
+/*
+go.mod file is important to maintain the dependencies of the project (Binds all the packages together)
+for example, we added the operations package using operations "Golang/Operations"
+If go.mod is not present, then we had to use full path of the that is "example.com/src/.../GoLang/Operations"
+*/
+
+/*
+If we have to create a stand alone go file then we have to use package main and main function in that file.
+
+This is due to Golangs file structure. As it work on packages, where each file must be part of a package.
+Golang doesnt have a concept of classes, so we have to use packages to group the functions together.
+
+Fact.. We cant files with differnt packages in the same directory. Each file of same directory must have the same package name.
+*/
