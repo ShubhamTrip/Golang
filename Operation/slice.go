0db5b506bba4d43d0bc22321508101be78c08006
Shubhamtrip <shubhamtrip16@gmail.com>
2025-03-04 02:24:33 +0530

    slice changed

--- Operation/slice.go
+++ Operation/slice.go
@@ -4,7 +4,15 @@ import "fmt"
 
 func sliceUse() {
 
-	var slice1 = []int{1, 2, 3, 4, 5}
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
 
-	fmt.Println("Slice 1: ", slice1)
+	myslice1 = append(myslice1, 20, 21)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myarr1 := [5]int{1, 2, 3, 4, 5}
+	myslice2 := myarr1[1:4]
+
+	fmt.Printf("myslice2 = %v\n", myslice2)
 }
