4beab5da15c7fe353b89655de764d988b6539732
Shubhamtrip <shubhamtrip16@gmail.com>
2025-03-04 01:06:38 +0530

    Added array

---
+++ Operation/array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var arr1 = [5]int{1, 2, 3, 4, 5}
+
+	fmt.Println("Array 1: ", arr1)
+
+	arr2 := [...]string{"apple", "mango", "banana", "grapes"}
+
+	fmt.Println("Array 2: ", arr2)
+
+	arr3 := [...]int{3: 10, 5: 50}
+
+	fmt.Println("Array 3: ", arr3)
+
+}
