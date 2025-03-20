ed031582ff9693b7f4d09487140b009baf53f523
Shubhamtrip <shubhamtrip16@gmail.com>
2025-03-20 23:37:26 +0530

    some chag

--- Closures/closure.go
+++ Closures/closure.go
@@ -5,6 +5,8 @@ import "fmt"
 // a closure is a function that captures and remembers variables from its surrounding scope,
 // even after the scope has exited. Closures are useful for maintaining state and creating function factories.
 
+// Just remember for now. And figure out the mechanism behind.
+// Use case: State management -- State transfer across function.
 func counter() func() int {
 
 	count := 0
